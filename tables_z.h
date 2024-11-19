9218223fa5480b5d95402be956e80505832cf04d
lkotipal <leo.kotipalo@helsinki.fi>
2024-11-19 13:45:32 +0200

    Z-curve table

---
+++ tables_z.h
@@ -0,0 +1,16 @@
+static unsigned const data3d [] = {
+0, 1, 2, 3, 4, 5, 6, 7, 
+};
+
+static unsigned const state3d [] = {
+0, 0, 0, 0, 0, 0, 0, 0, 
+};
+
+static unsigned const idata3d [] = {
+0, 1, 2, 3, 4, 5, 6, 7, 
+};
+
+static unsigned const istate3d [] = {
+0, 0, 0, 0, 0, 0, 0, 0, 
+};
+
